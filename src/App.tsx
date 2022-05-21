6385cacfb1cb98c9a101402051e0439d1abf246b
hireoochen@gmail.com <hireoochen@gmail.com>
2022-05-21 14:24:01 +0800

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+
+// Tailwind CSS 
+import './theme/tailwind.css';
+
 setupIonicReact();
 
 const App: React.FC = () => (
